4f22817c74daa5cd89d6b8309ee10144ac4d2570
Borna Šepić <borna@vaangroup.com>
2025-06-09 20:13:54 +0200

    Make registration route public

--- main/src/users/users.controller.ts
+++ main/src/users/users.controller.ts
@@ -4,11 +4,13 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from '../decorators/user.decorator';
 import { NullableUser } from './entities/user.entity';
+import { Public } from '../auth/public.decorator';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) { }
 
+  @Public()
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
